cefc1147987e040e97367439a06083857250d994
Bckymoon <bckymoon@hotmail.com>
2020-07-16 20:40:04 -0300

    att

--- scripts.js
+++ scripts.js
@@ -3,5 +3,7 @@ const canvas = document.querySelectorAll("[data-tipoGrafico]");
 let dado = {
     titulo: "Temperatura em C°",
     legendaX: ["","","","","",""],
-    temperatura: []
+    temperatura: [],
+    min: "0",
+    max: "40"
 }
